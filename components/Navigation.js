a7f5e9fe7902404889bdfb61dee4a70ce6fee3c5
Vitaly Vorobyev <vitaly.vorobyev@cern.ch>
2025-03-07 20:25:45 +0100

    Navigation fixed

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -15,46 +14,33 @@ const Stack = createNativeStackNavigator();
 
 export default Navigation = () => {
   const { isLoading, firstName, avatar, lastName, email } = useUserData();
-  const [screens, setScreens] = useState([]);
-  const [initialRouteName, setInitialRouteName] = useState(null);
-
-  useEffect(() => {
-    const getHeader = (props) => <Header {...props} avatar={avatar} firstName={firstName} lastName={lastName} />;
-    if (firstName && email) {
-      setInitialRouteName('Home');
-      setScreens([
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerTitle: getHeader,
-            headerStyle: { height: 100 },
-          }}
-        />,
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{headerTitle: getHeader,}}
-        />
-      ]);
-    } else {
-      setInitialRouteName('Onboarding');
-      setScreens([
-        <Stack.Screen
-          name="Onboarding"
-          component={OnboardingScreen}
-          options={{ headerShown: false }} />
-      ]);
-    }
-  }, [firstName, avatar, lastName, email]);
 
   if (isLoading) {
     return <SplashScreen />;
   }
 
+  // Determine if the user is logged in.
+  const isLoggedIn = firstName && email;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialRouteName}>{screens}</Stack.Navigator>
+      {isLoggedIn ? (
+        <Stack.Navigator
+          screenOptions={{
+            headerTitle: (props) => (
+              <Header {...props} avatar={avatar} firstName={firstName} lastName={lastName} />
+            ),
+            headerStyle: { height: 100 },
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
